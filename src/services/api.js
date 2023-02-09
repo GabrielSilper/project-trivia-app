039b0dfc6481cb224381e4f8b0056590df03d14f
Douglas Santos <87917605+DougSaint@users.noreply.github.com>
2023-02-09 19:21:02 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export const getQuestions = async (token) => {
-  const BASEURL = `https://opentdb.com/api.php?amount=${amount}&token=${token}`;
+  const BASEURL = `https://opentdb.com/api.php?amount=5&token=${token}`;
   try {
     const result = await fetch(BASEURL);
     const data = result.json();
