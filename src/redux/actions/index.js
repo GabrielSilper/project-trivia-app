ca01b625ae77b67c5f1d4fbcb73f1176877d9d65
GabrielSilper <gabrielsilper.main@gmail.com>
2023-02-10 15:01:07 -0400

    Requisito 9 - Resolvido

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,12 @@
 export const DADOS_LOGIN = 'DADOS_LOGIN';
+export const ATUALIZA_SCORE = 'ATUALIZA_SCORE';
 
 export const enviaDadosLogin = (payload) => ({
   type: DADOS_LOGIN,
+  payload,
+});
+
+export const atualizaScore = (payload) => ({
+  type: ATUALIZA_SCORE,
   payload,
 });
