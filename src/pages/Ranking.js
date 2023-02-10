a1f9742c3b6773ebeab6a44e277ed6e6e56c2164
Lucas <lm.spizzirri@gmail.com>
2023-02-10 15:52:56 -0300

    Req 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
+
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ () => {
+            history.push('/');
+          } }
+        >
+          Início
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.func.isRequired,
+};
